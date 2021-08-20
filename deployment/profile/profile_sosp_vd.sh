dc71b7b259f4be445b54f1ee3cb7769c122dd007
Yicheng Shen <46360706+YichengShen@users.noreply.github.com>
2021-08-20 08:30:47 +0800

    Update profile_sosp_vd.sh

--- deployment/profile/profile_sosp_vd.sh
+++ deployment/profile/profile_sosp_vd.sh
@@ -4,16 +4,16 @@
 # step 1. run the configuration below through multiple.sh
 
 RCLogLevel=warn
-Rabia_ClosedLoop=false
+Rabia_ClosedLoop=true
 Rabia_ClientDistributingMethod=modulo
 
 NServers=3
 NFaulty=1
-NClients=15
+NClients=200
 NConcurrency=1
 ClientTimeout=120
 ClientThinkTime=0
-ClientBatchSize=20
-ProxyBatchSize=15
+ClientBatchSize=10
+ProxyBatchSize=20
 ProxyBatchTimeout=5000000
-NClientRequests=1000
+NClientRequests=0
