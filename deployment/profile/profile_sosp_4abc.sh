8f6e898e472ce5c0699d6afa5ea0fdaddb9b62df
Haochen Pan <haochen.pan@bc.edu>
2021-09-11 20:31:14 -0400

    update 4abc.sh and 4d.sh
    Rabia_ClientDistributingMethod=ad-hoc

--- deployment/profile/profile_sosp_4abc.sh
+++ deployment/profile/profile_sosp_4abc.sh
@@ -8,11 +8,10 @@
 
 RCLogLevel=warn
 Rabia_ClosedLoop=true
-Rabia_ClientDistributingMethod=modulo
+Rabia_ClientDistributingMethod=ad-hoc
 
 NServers=3
 NFaulty=1
-NClients=20 # 40, 60, 80, 100, 200, 300, 400, 500
 NConcurrency=1
 ClientTimeout=120
 ClientThinkTime=0
@@ -20,3 +19,40 @@ ClientBatchSize=10
 ProxyBatchSize=20
 ProxyBatchTimeout=5000000
 NClientRequests=0
+
+
+# 20 clients, comments out other 2-line blocks except this one
+NClients=20
+Rabia_ClientsPerServer=(20 0 0)
+
+# 40 clients, comments out other 2-line blocks except this one 
+# NClients=40
+# Rabia_ClientsPerServer=(20 20 0)
+
+# 60 clients, comments out other 2-line blocks except this one 
+# NClients=60
+# Rabia_ClientsPerServer=(20 20 20)
+
+# 80 clients, comments out other 2-line blocks except this one 
+# NClients=80
+# Rabia_ClientsPerServer=(40 20 20)
+
+# 100 clients, comments out other 2-line blocks except this one 
+# NClients=100
+# Rabia_ClientsPerServer=(40 40 20)
+
+# 200 clients, comments out other 2-line blocks except this one 
+# NClients=200
+# Rabia_ClientsPerServer=(80 60 60)
+
+# 300 clients, comments out other 2-line blocks except this one 
+# NClients=300
+# Rabia_ClientsPerServer=(100 100 100)
+
+# 400 clients, comments out other 2-line blocks except this one 
+# NClients=400
+# Rabia_ClientsPerServer=(140 140 120)
+
+# 500 clients, comments out other 2-line blocks except this one 
+# NClients=500
+# Rabia_ClientsPerServer=(180 160 160)
